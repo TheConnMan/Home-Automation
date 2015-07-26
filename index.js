2b20013476a74f2bf6ab015843fbce035c29720d
TheConnMan <bcconn2112@gmail.com>
2015-07-26 12:13:05 -0400

    Send open or close message based on input action.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var APP_ID = undefined;
 
+var OPEN_COMMANDS = ['open'];
+var CLOSE_COMMANDS = ['close', 'shut'];
+
 var AlexaSkill = require('./AlexaSkill');
 var config = require('./config');
 var request = require('sync-request');
@@ -27,16 +30,36 @@ EchoBlinds.prototype.eventHandlers.onSessionEnded = function(sessionEndedRequest
 
 EchoBlinds.prototype.intentHandlers = {
 	EchoBlindsIntent: function(intent, session, response) {
-
+		var action = intent.slots.action.value;
+		var payload = createPayload(action);
+		if (payload) {
+			sendMessage(payload);
+			response.tell('Message sent');
+		} else {
+			response.tell('Sorry, ' + action + ' is not a valid command');
+		}
 	},
 	HelpIntent: function(intent, session, response) {
 		response.ask('Would you like to open or close the blinds?');
 	}
 };
 
+function createPayload(action) {
+	if (OPEN_COMMANDS.indexOf(action) !== -1) {
+		return {
+			open: true
+		};
+	} else if (CLOSE_COMMANDS.indexOf(action) !== -1) {
+		return {
+			open: false
+		};
+	}
+}
+
 function sendMessage(payload) {
-	var url = config.host + '/api/v1/put?key=' + config.apiKey + '&clientId=' + config.clientId + '&payload=' + JSON.stringify(payload);
-	request('GET', url);
+	var url = config.host + '/api/v1/put?key=' + config.apiKey + '&clientId=' + config.clientId + '&payload=' + encodeURIComponent(JSON.stringify(payload));
+	var res = request('GET', url);
+	console.log(res.getBody('utf8'));
 }
 
 exports.handler = function(event, context) {
