95296b0bd6b6f9c1826f2489410ae80fa1878a82
TheConnMan <bcconn2112@gmail.com>
2015-07-26 00:05:56 -0400

    Initial commit of the Alexa Skill framework.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Echo Blinds Skill
+Alexa Skill for opening and closing blinds using [https://github.com/TheConnMan/Message-Relay](https://github.com/TheConnMan/Message-Relay) and [https://github.com/TheConnMan/EchoBlinds](https://github.com/TheConnMan/EchoBlinds)
---
+++ index.js
@@ -0,0 +1,41 @@
+var APP_ID = undefined;
+
+var AlexaSkill = require('./AlexaSkill');
+
+var EchoBlinds = function() {
+	AlexaSkill.call(this, APP_ID);
+};
+
+EchoBlinds.prototype = Object.create(AlexaSkill.prototype);
+EchoBlinds.prototype.constructor = EchoBlinds;
+
+EchoBlinds.prototype.eventHandlers.onSessionStarted = function(sessionStartedRequest, session) {
+	console.log('EchoBlinds onSessionStarted requestId: ' + sessionStartedRequest.requestId + ', sessionId: ' + session.sessionId);
+};
+
+EchoBlinds.prototype.eventHandlers.onLaunch = function(launchRequest, session, response) {
+	console.log('EchoBlinds onLaunch requestId: ' + launchRequest.requestId + ', sessionId: ' + session.sessionId);
+	var speechOutput = 'Welcome to Echo Blinds. What would you like to do?';
+	response.ask(speechOutput);
+};
+
+EchoBlinds.prototype.eventHandlers.onSessionEnded = function(sessionEndedRequest, session) {
+	console.log('EchoBlinds onSessionEnded requestId: ' + sessionEndedRequest.requestId + ', sessionId: ' + session.sessionId);
+};
+
+EchoBlinds.prototype.intentHandlers = {
+	EchoBlindsIntent: function(intent, session, response) {
+
+	},
+	MessageIntent: function(intent, session, response) {
+
+	},
+	HelpIntent: function(intent, session, response) {
+		response.ask('Would you like to open or close the blinds?');
+	}
+};
+
+exports.handler = function(event, context) {
+	var echoBlinds = new EchoBlinds();
+	echoBlinds.execute(event, context);
+};
